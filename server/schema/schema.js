9788acb111099c80d2b302819709fd690ccbce0d
vipinyadav610 <adi19910810@gmail.com>
2018-02-09 00:41:55 +0530

    schema implemented

---
+++ server/schema/schema.js
@@ -0,0 +1,61 @@
+import {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} from 'graphql'
+import _ from 'lodash'
+const companies = [
+    { id: '1', name: 'Astics', description: 'company a' },
+    { id: '2', name: 'More binary', description: 'company b' },
+    { id: '3', name: 'facebook', description: 'company c' },
+    { id: '4', name: 'amelco', description: 'company d' },
+    { id: '5', name: 'google', description: 'company e' }
+]
+
+const users = [
+    { id: '1', firstName: 'gaurav', age: 26, companyID: '1' },
+    { id: '2', firstName: 'rishabh', age: 25, companyID: '5' },
+    { id: '3', firstName: 'prashant', age: 24, companyID: '2' },
+    { id: '4', firstName: 'ryan', age: 23, companyID: '4' },
+    { id: '5', firstName: 'vaidehi', age: 22, companyID: '3' }
+]
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: {
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString }
+    }
+})
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyID: {
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return _.find(companies, { id: parentValue.companyID })
+            }
+        }
+    }
+})
+const RootQuery = new GraphQLObjectType({
+    name: 'user',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, { id }) {
+                return _.find(users, { id })
+            }
+        }
+    }
+})
+export default new GraphQLSchema({
+    query: RootQuery
+
+})
--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 import express from 'express'
 import expressGraphQL from 'express-graphql'
 import mongoose from 'mongoose'
-
+import schema from './schema/schema'
 const app = express();
-const MONGO_URI = `mongodb://vipin10:vipin123@ds125578.mlab.com:25578/efrsgf`
-mongoose.Promise = global.Promise
-mongoose.connect(MONGO_URI);
-mongoose.connection
-    .once('open', () => console.log('Connected to MongoLab instance.'))
-    .on('error', error => console.log('Error connecting to MongoLab:', error))
+
+// const MONGO_URI = `mongodb://vipin10:vipin123@ds125578.mlab.com:25578/efrsgf`
+// mongoose.Promise = global.Promise
+// mongoose.connect(MONGO_URI);
+// mongoose.connection
+//     .once('open', () => console.log('Connected to MongoLab instance.'))
+//     .on('error', error => console.log('Error connecting to MongoLab:', error))
 
 app.use('/graphql', expressGraphQL({
+    schema,
     graphiql: true
 }));
 
